fb6d078566ba51925971548a30daafb41ada0279
Roma1102 <156461295+Roma1102@users.noreply.github.com>
2024-07-29 07:51:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#from time import time
+
+
+#def countdown(name, seconds):
+  #import time
+
+  #for sec in range(seconds, -1, -1):
+    #if sec > 0:
+     # print(f'the rocket {name} will start by {sec} seconds')
+  #  else:
+     # print('Lift off')
+  #time.sleep(1)
+
+
+#countdown('Falcon 2000', 5)
+#def all_sum(*numbers):
+  print(numbers)
+  resulit = 0
+
+  for num in numbers:
+    resulit += num
+
+
+  return resulit
+
+print(all_sum(1, 2, 3, 100, 214257745847, 56, 455454682142525445454444545455825742541433))
